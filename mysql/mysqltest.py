9ac66caed6b3ddc8bc398dc3cd14e97a38d83831
Sagar Karandikar <karandikarsagar@gmail.com>
2012-07-09 14:38:58 -0700

    relevant info now logged to file

--- mysql/mysqltest.py
+++ mysql/mysqltest.py
@@ -55,11 +55,14 @@ TEST_URL = "jdbc:mysql://localhost/grindertest?user=root&password=toor"
 #initialize db driver
 initialize_driver()
 experiment_clean()
-datagen = TSdata(10000, 100, range(80, 120, 1))
+points = 10000
+streams = 100
+valid_range = range(80, 120, 1)
+datagen = TSdata(points, streams, valid_range)
 
 class TestRunner:
     def __init__(self):
-        grinder.logger.info("Started Logging")
+        grinder.logger.info("Started Logging: " + str(points) + " points each for " + str(streams) + " streams at " + str(time.time()) + " seconds since the epoch.")
 
     def __call__(self):
         #start this round
@@ -73,12 +76,13 @@ class TestRunner:
                 query_end += str(tup) + ','
         query_end = query_end[:-1]
         testInsert.executeUpdate("insert into grindertest values " + query_end)
+        grinder.logger.info("Added " + str(streams) + " points at " + str(time.time()))
         
 
-        grinder.logger.info(str(time.time()))
         a = testQuery.executeQuery("select * from grindertest")
         while(a.next()):
-            print("[" + a.getString("streamid") + " " + a.getString("time") + " " + a.getString("value") + "]")
+            temp = ("[" + a.getString("streamid") + " " + a.getString("time") + " " + a.getString("value") + "]") #no need to waste time actually printing
+        grinder.logger.info("Fetched all points at " + str(time.time()))
 
         ensureClosed(conn)
         ensureClosed(s)
