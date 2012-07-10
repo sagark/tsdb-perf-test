6acf965af8926c91ef768e18a46d508445c5e3bb
Sagar Karandikar <karandikarsagar@gmail.com>
2012-07-10 12:23:39 -0700

    cleanup

--- mysql/mysqltest.py
+++ mysql/mysqltest.py
@@ -18,15 +18,17 @@ from t_mysql import MySQLAccess
 test1 = Test(1, "Database insert")
 test2 = Test(2, "Database query")
 
-points = 10000
-streams = 100
-valid_range = range(80, 120, 1)
-datagen = TSdata(points, streams, valid_range)
-
 class TestRunner:
     def __init__(self):
-        grinder.logger.info("Started Logging: " + str(points) + " points each for " + str(streams) + " streams at " + str(time.time()) + " seconds since the epoch.")
+        grinder.logger.info("Started Logging: " + str(points) + 
+                            " points each for " + str(streams) + 
+                            " streams at " + str(time.time()) + 
+                            " seconds since the epoch.")
         self.testdb = MySQLAccess()
+        self.points = 10000
+        self.streams = 100
+        self.valid_range = range(80, 120, 1)
+        self.datagen = TSData(self.points, self.streams, self.valid_range)
 
     def __call__(self):
         #start this round
@@ -42,12 +44,17 @@ class TestRunner:
                 query_end += str(tup) + ','
         query_end = query_end[:-1]
         testInsert.executeUpdate("insert into grindertest values " + query_end)
-        grinder.logger.info("Added " + str(streams) + " points at " + str(time.time()))
+        grinder.logger.info("Added " + str(streams) + " points at " + 
+                                                               str(time.time()))
         
 
         a = testQuery.executeQuery("select * from grindertest")
         while(a.next()):
-            temp = ("[" + a.getString("streamid") + " " + a.getString("time") + " " + a.getString("value") + "]") #no need to waste time actually printing
+            temp = ("[" + a.getString("streamid") + " " + a.getString("time") + 
+                    " " + a.getString("value") + "]") 
+        #no need to waste time actually printing
+
+
         grinder.logger.info("Fetched all points at " + str(time.time()))
 
 
