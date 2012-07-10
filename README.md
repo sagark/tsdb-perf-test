27d2412543ab7357a64197d6dc5921bc99698750
Sagar Karandikar <karandikarsagar@gmail.com>
2012-07-10 12:49:32 -0700

    more cleanup, better readme

--- README.md
+++ README.md
@@ -7,12 +7,39 @@ Performance Tests for Time Series Databases
 Setting up Grinder
 ==================
 
-This is largely relevant for installing grinder:
-http://tompurl.com/2011/02/13/installing-the-grinder-on-ubuntu-10-10/
+See setupguide
 
+Folder Structure
+================
+
+Because I didn't want to waste even more time dealing with jython and grinder's
+path system, I instead resorted to symlinks. Here's a breakdown of where things
+are, starting from project root:
+
+Folders
+/<DBNAME>/
+    These are folders specific to each database being tested. Each contains a
+    t_databasename.py file that extends outline.py in root. This abstracts away
+    the database queries, setup, etc. for the tests.
+    
+    Each folder also contains a symlink to each type of test from /tests/, a
+    grinder.properties file for configuring/selecting tests and a symlink to
+    datagenerator.py (in root).
+
+    Finally, each folder contains a first_run file that describes first-time db
+    setup instructions.
+
+/bin/
+    Contains db drivers
+
+/tests/
+    Contains the various tests
 
 Running Tests
 =============
+
+cd into the folder for the DB you wish to test, set the script in
+grinder.properties and run:
 
 Start the console:
 java net.grinder.Console
--- tests/insertquery.py
+++ tests/insertquery.py
@@ -20,22 +20,22 @@ test2 = Test(2, "Database query")
 
 class TestRunner:
     def __init__(self):
-        grinder.logger.info("Started Logging: " + str(points) + 
-                            " points each for " + str(streams) + 
-                            " streams at " + str(time.time()) + 
-                            " seconds since the epoch.")
         self.testdb = MySQLAccess()
         self.points = 10000
         self.streams = 100
         self.valid_range = range(80, 120, 1)
-        self.datagen = TSData(self.points, self.streams, self.valid_range)
+        self.datagen = TSdata(self.points, self.streams, self.valid_range)
+        grinder.logger.info("Started Logging: " + str(self.points) + 
+                            " points each for " + str(self.streams) + 
+                            " streams at " + str(time.time()) + 
+                            " seconds since the epoch.")
 
     def __call__(self):
         #start this round
         conn, s = self.testdb.dbconn, self.testdb.dbstate
         logconn, logstate = self.testdb.dbaboutconn, self.testdb.dbaboutstate
 
-        roundvals = datagen.next()
+        roundvals = self.datagen.next()
         testInsert = test1.wrap(s)
         testQuery = test2.wrap(s)
 
@@ -44,7 +44,7 @@ class TestRunner:
                 query_end += str(tup) + ','
         query_end = query_end[:-1]
         testInsert.executeUpdate("insert into grindertest values " + query_end)
-        grinder.logger.info("Added " + str(streams) + " points at " + 
+        grinder.logger.info("Added " + str(self.streams) + " points at " + 
                                                                str(time.time()))
         
 
