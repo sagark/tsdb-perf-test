7e28ffe853481853e5208f4fc2323ccc4f471695
Sagar Karandikar <karandikarsagar@gmail.com>
2012-07-12 10:55:58 -0700

    split graphs, now take in arbitrary number of logs

--- logparse/logparser.py
+++ logparse/logparser.py
@@ -3,65 +3,86 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 import numpy as np
 
-filename = sys.argv[1]
+#filename = sys.argv[1]
 
-log = file(filename)
-lines = log.readlines()
+DB_NAMES = ["mysql", "opentsdb", "postgres", "readingdb", "scidb"]
+formatiter = iter(['r-', 'g-', 'r-', 'b-', 
+                            'k-', 'b-'])
 
-while "Started Logging" not in lines[0]:
-    lines.pop(0)
 
-#get title
-title = lines.pop(0).split("Started Logging: ")[1].split(' at')[0]
 
-while "[ run-0 ]" not in lines[0]:
-    lines.pop(0)
+def parsedata(filearg):
+    log = file(filearg)
+    lines = log.readlines()
+    log.close()
 
-print(lines[0])
+    while "Started Logging" not in lines[0]:
+        lines.pop(0)
 
-points = []
+    #get title
+    title = lines.pop(0).split("Started Logging: ")[1].split(' at')[0]
 
-counter = 0
-while "finished" not in lines[0]:
-    insert = lines.pop(0).split(": ")[2]
-    query = lines.pop(0).split(": ")[2]
-    size = lines.pop(0).split("now ")[1].replace(" bytes.", "")
-    #print(insert)
-    #print(query)
-    #print(size)
-    points.append([counter, eval(insert), eval(query), eval(size)])
-    counter += 10000 #number of records before each round
+    while "[ run-0 ]" not in lines[0]:
+        lines.pop(0)
 
-graphthis = []
-for x in points:
-    addtog = []
-    addtog.append(x[0]) #number of points in db already
-    addtog.append(x[1][2]) #time to insert
-    addtog.append(x[2][2]) #time to query all
-    addtog.append(x[3]/1000000) #db size after completion, convert to MB
-    graphthis.append(addtog)
+    #print(lines[0])
 
-a = np.array(graphthis)
-print(a)
-print(a[:,0])
+    points = []
 
-x = a[:,0]
-y1 = a[:,1]
-y2 = a[:,2]
-y3 = a[:,3]
+    counter = 0
+    while "finished" not in lines[0]:
+        insert = lines.pop(0).split(": ")[2]
+        query = lines.pop(0).split(": ")[2]
+        size = lines.pop(0).split("now ")[1].replace(" bytes.", "")
+        points.append([counter, eval(insert), eval(query), eval(size)])
+        counter += 10000 #number of records before each round
+
+    graphthis = []
+    for x in points:
+        addtog = []
+        addtog.append(x[0]) #number of points in db already
+        addtog.append(x[1][2]) #time to insert
+        addtog.append(x[2][2]) #time to query all
+        addtog.append(x[3]/1000000) #db size after completion, convert to MB
+        graphthis.append(addtog)
+
+    a = np.array(graphthis)
+
+    for name in DB_NAMES:
+        if name in filearg:
+            a = (name, a)
+
+
+    return a
+
+db_arrays = []
+for x in sys.argv[1:]:
+    db_arrays.append(parsedata(x))
 
 fig = plt.figure()
-ax1 = fig.add_subplot(111)
-plt.plot(x, y1, 'g-', x, y2, 'r-')
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
 ax1.set_xlabel('# of Records in DB')
 ax1.set_ylabel('Time for operation completion (s)')
-
-ax2 = ax1.twinx()
-ax2.plot(x, y3, 'b-')
+ax2.set_xlabel('# of Records in DB')
 ax2.set_ylabel('DB size (MB)')
 
-leg = ax1.legend(('Insertion', 'Query'),
-           'upper center', shadow=True)
-leg2 = ax2.legend(('DB Size',), 'upper left', shadow=True)
+legend1 = ()
+legend2 = ()
+
+for a in db_arrays:
+    name = a[0]
+    a = a[1]
+    x = a[:,0]
+    y1 = a[:,1]
+    y2 = a[:,2]
+    y3 = a[:,3]
+    ax1.plot(x, y1, formatiter.next(), x, y2, formatiter.next())
+    ax2.plot(x, y3, formatiter.next())
+    legend1 += ('Insertion (' + name + ')', 'Query (' + name + ')')
+    legend2 += (name,)
+
+leg = ax1.legend(legend1, 'upper left', shadow=True)
+leg2 = ax2.legend(legend2, 'upper left', shadow=True)
 
 plt.savefig('test.png')
