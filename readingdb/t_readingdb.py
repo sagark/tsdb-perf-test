e5c91f0ba5349f350ab24a9355f60d5c1e64ad80
Sagar Karandikar <karandikarsagar@gmail.com>
2012-07-13 10:30:03 -0700

    readingdb db size should work now, new logs

--- readingdb/t_readingdb.py
+++ readingdb/t_readingdb.py
@@ -5,6 +5,8 @@ from java.lang import *
 import time
 import sys
 import subprocess
+import glob
+import shlex
 
 #project specific
 from framework import DBTest
@@ -52,14 +54,31 @@ class ReadingDBAccess(DBTest):
     def get_db_size(self):
         #######################Need to figure out what goes here.
         f = file('dbsize', 'w')
-        subprocess.call(['du', '-s', "--exclude='/var/lib/readingdb/log*'", 
-                                            '/var/lib/readingdb/'], stdout=f)
+        
+        # for some reason glob and regex don't work in grinder, though they do
+        # in jython, just hardcode it
+        #cmd = """du -c /var/lib/readingdb/__* /var/lib/readingdb/read*"""
+        #arg = shlex.split(cmd)
+        #command = arg[:-2] + glob.glob(arg[-2]) + glob.glob(arg[-1])
+        command = ['du', '-c', '/var/lib/readingdb/__db.005', 
+        '/var/lib/readingdb/__db.002', '/var/lib/readingdb/__db.003', 
+        '/var/lib/readingdb/__db.006', '/var/lib/readingdb/__db.001', 
+        '/var/lib/readingdb/__db.004', '/var/lib/readingdb/readings-2.db', 
+        '/var/lib/readingdb/readings-7.db', '/var/lib/readingdb/readings-4.db', 
+        '/var/lib/readingdb/readings-5.db', '/var/lib/readingdb/readings-9.db', 
+        '/var/lib/readingdb/readings-3.db', '/var/lib/readingdb/readings-1.db', 
+        '/var/lib/readingdb/readings-0.db', '/var/lib/readingdb/readings-6.db', 
+        '/var/lib/readingdb/readings-8.db']
+
+        a = subprocess.call(command, stdout=f)
         f.close()
         f = file('dbsize')
         #f = file('dbsize')
-        a = f.read()
+        a = f.readlines()
+        a = a[-1]
         f.close()
-        a = a.split('\t/')[0]
+
+        a = a.split('\t')[0]
         a = int(a)*1000
         return str(a)
 
