60244103da4af3304eb74500e4aa5e439e57075f
Sagar Karandikar <karandikarsagar@gmail.com>
2012-07-23 12:40:19 -0700

    added sample configs

---
+++ mysql/sample_dbconfig.py
@@ -0,0 +1,7 @@
+#set the config in this file and rename it to dbconfig.py
+
+class DBconfig:
+    db = "grindertest"
+    dbabout = "information_schema"
+    user = "root"
+    passw = "toor"
---
+++ postgres/sample_dbconfig.py
@@ -0,0 +1,8 @@
+#set the db config in this file and rename it to dbconfig.py
+
+class DBconfig:
+    db = "grindertest"
+    dbabout = "information_schema"
+    user = "postgres"
+    passw = "postgres"
+    port = "5433"
