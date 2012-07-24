03a10d7723369aa6c1c19d06b1cf820949963f3f
Sagar Karandikar <karandikarsagar@gmail.com>
2012-07-24 11:18:21 -0700

    cleanup query_perf

--- tests/query_perf.py
+++ tests/query_perf.py
@@ -20,16 +20,17 @@ exec(inp)
 class TestRunner:
     def __init__(self):
         self.testdb = DBAccess()
-        self.streams = 100 #number of streams
-        self.pps = 100000 #points per stream
-        logstr = self.testdb.init_insert(self.streams, self.pps, False)
+        self.t1size = 0 #size of query for first non-control test
+        self.streams = 10 #number of streams
+        self.pps = 20000 #points per stream
+        logstr = self.testdb.init_insert(self.pps, self.streams, False)
         grinder.logger.info(logstr)
         self.callcounter = 0 #start the test callcounter
         counter = 0
         #prepare by inserting large number of points
         while True:
             try:
-                #print("inserting " + str(counter))
+                print("inserting round: " + str(counter))
                 res = self.testdb.run_insert_h()
                 grinder.logger.info("Insertion Results as (start time, end time, "
                                 "completion" + 
@@ -39,7 +40,7 @@ class TestRunner:
                 grinder.logger.info("The database size is now " + size + " bytes.")
 
                 self.testdb.reset_conn_state()
-                #counter += 1
+                counter += 1
             except StopIteration:
                 # the prep is complete
                 grinder.logger.info("Insertion finished at: " + str(time.time())) 
@@ -49,13 +50,14 @@ class TestRunner:
                 break
 
     def __call__(self):
-        if self.callcounter > 100:
+        if self.callcounter >= 100:
             grinder.logger.info("Test complete")
             self.testdb.close_all()
             grinder.stopThisWorkerThread()
 
         else:
             self.callcounter += 1 
+            self.t1size += 10
         #start this round
 
         # this is the control. If the time that this takes varies, we know that
@@ -70,8 +72,9 @@ class TestRunner:
         # actual experiment. the values for this will vary over time
         # this represents querying for data from an arbitrary time window of
         # fixed width
-        res = self.testdb.query(10000, self.streams)
-        grinder.logger.info("Query     Results as (start time, end time, "
+        # measure performance vs query size
+        res = self.testdb.query(self.t1size, self.streams)
+        grinder.logger.info("Query " + str(self.t1size) + " items Results as (start time, end time, "
                             "completion" + 
                             " time): (" + str(res[0]) + ", " + str(res[1]) + 
                             ", " + str(res[2]) + ")")
