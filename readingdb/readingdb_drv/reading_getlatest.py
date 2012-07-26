89b121c7acc413c8fc3dc451ffffd551c82c396a
Sagar as root@verde.cs.berkeley.edu <root@verde.cs.berkeley.edu>
2012-07-26 10:06:27 -0700

    readingdb latestpoint fix, new postgres log

--- readingdb/readingdb_drv/reading_getlatest.py
+++ readingdb/readingdb_drv/reading_getlatest.py
@@ -8,7 +8,7 @@ import time
 rdb.db_setup('localhost', 4242)
 a = rdb.db_open('localhost')
 
-b = rdb.db_prev(1, 100000000000000, conn=a)
+b = rdb.db_prev(1, 100000000000, conn=a)
 lasttime = b[0][0][0]
 
 rdb.db_close(a)
