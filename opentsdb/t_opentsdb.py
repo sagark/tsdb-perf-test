3a2880d2f949fabd1bf324c72141c176cfaa4796
Sagar Karandikar <karandikarsagar@gmail.com>
2012-07-31 10:31:08 -0700

    added db_size to opentsdb

--- opentsdb/t_opentsdb.py
+++ opentsdb/t_opentsdb.py
@@ -21,10 +21,7 @@ class OpenTSDBAccess(DBTest):
         #general properties
         #N/A
 
-        #setup driver(s)
-        self.driver_simple = "readingdb_drv/reading_simple.py"
-        self.driver_complex = "readingdb_drv/reading_complex.py"
-    
+
         #start connection/statement
         self.reset_conn_state() #THIS IS REQUIRED BEFORE self.prepare()
 
@@ -45,14 +42,38 @@ class OpenTSDBAccess(DBTest):
         pass
 
     def get_db_size(self):
-        # possibly try hbase shell status 'detailed'
-        dbsize = 3
-        return str(dbsize) #go back to str
+        totalsize = 0
+        props = {'tsdb': [], 'tsdb-uid': []}
+        a = subprocess.Popen(["hbase", "shell", "opentsdb_drv/db_size"], stdout = subprocess.PIPE)
+        b = a.communicate()
+        cleanup = b[0].split()
+        while 'tsdb' not in cleanup[0]:
+            cleanup.pop(0)
+        cleanup.pop(0)
+        while 'tsdb' not in cleanup[0]:
+            tmp = cleanup.pop(0)
+            if 'Size' in tmp:
+                props['tsdb'].append(tmp)
+        cleanup.pop(0)
+        for tmp in cleanup:
+            if 'Size' in tmp:
+                props['tsdb-uid'].append(tmp)
+        #print(props['tsdb'])
+        allprops = props['tsdb'] + props['tsdb-uid']
+        for x in allprops:
+            x = x.replace(',', '')
+            x = x.split('=')
+            if 'MB' in x[0]:
+                totalsize += int(x[1])*1000000 #convert to bytes
+            elif 'KB' in x[0]:
+                totalsize += int(x[1])*1000 #convert to bytes    
+        dbsize = totalsize
+        return str(dbsize)
 
     def prepare(self):
         devnull = open('/dev/null', 'w')
         # prepare by deleting all data files
-        # make sure readingdb_drv/prep_server has been chmod +x'd
+        # make sure opentsdb_drv/prep_server has been chmod +x'd
         # first we need to find out if we are running as root:
         try:
             urllib2.urlopen('http://localhost:4242/diediedie')
@@ -72,7 +93,8 @@ class OpenTSDBAccess(DBTest):
         if roundgen.pt_time == 946684800:
             #if pt_time is equal to the start, we need to make the streams
             for x in range(1, streamcount+1):
-                subprocess.call(['tsdb', 'mkmetric', 'stream'+str(x)], stdout = devnull, stderr = devnull)
+                subprocess.call(['tsdb', 'mkmetric', 'stream'+str(x)], 
+                                           stdout = devnull, stderr = devnull)
         completiontime = 0
         overallstart = time.time()
         for vallist in roundgen:
@@ -89,78 +111,24 @@ class OpenTSDBAccess(DBTest):
  
         return [overallstart, endtime, completiontime]
 
-    def run_insert_h(self):
-        #mkmetric as we go
-        #should be fairly well-optimized at this point
-        #special height-wise insert for readingdb
-        roundgen = self.insertGenerator.next() #potential StopIteration()
-        
-
-        
-        return returnlist
 
 
     def run_query_all(self, debug=False):
+        returnlist = [1, 1, 1]
+        #stuff here
         if debug:
-            a = subprocess.Popen(["readingdb_drv/run_query_all.py", "'True'"], stdout=subprocess.PIPE)
-        else:
-            a = subprocess.Popen(["readingdb_drv/run_query_all.py", "'False'"], stdout=subprocess.PIPE)
-        b = a.communicate()[0]
-        returnlist = eval(b)
-
-        if debug:
-            debugout = file('tempfiles/debugout')
-            dout = debugout.readlines()
-            debugout.close()
             debugout = []
-            for row in dout:
-                debugout += eval(row)
             return debugout
-
 
         return returnlist
        
 
     def query(self, records, streams, debug=False):
         """Query "records" records from "streams" streams"""
-        
-        ##getting latest point in db
-        a = subprocess.Popen(["readingdb_drv/reading_getlatest.py"], stdout=subprocess.PIPE)
-        b = a.communicate()[0]
-        last = int(eval(b))
-    
-        #latest = file('tempfiles/lasttime')
-        #last = int(eval(latest.read()))
-        #latest.close()
-        #print(last)
-        lastpossible = last - records + 1
-        default_starttime = 946684800
-
-        if default_starttime >= lastpossible:
-            print("WARNING: timerange starts before earliest, resorting to" + 
-                    " forced lastpossible")
-            qstarttime = lastpossible
-        else:  
-            qstarttime = random.randrange(default_starttime, lastpossible)
-
-        qendtime = qstarttime + records 
-        params = [streams, qstarttime, qendtime]
-        
+        returnlist = [1, 1, 1]
+        #stuff here
         if debug:
-            c = subprocess.Popen(["readingdb_drv/query.py", str(params), "'True'"], stdout=subprocess.PIPE)
-        else:
-            c = subprocess.Popen(["readingdb_drv/query.py", str(params), "'False'"], stdout=subprocess.PIPE)          
-        d = c.communicate()[0]
-        returnlist = eval(d)
-
-        if debug:
-            debugout = file('tempfiles/debugout')
-            dout = debugout.readlines()
-            debugout.close()
             debugout = []
-            for row in dout:
-                debugout += eval(row)
             return debugout
 
-        
         return returnlist
